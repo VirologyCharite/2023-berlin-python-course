cc75e8147d869bfde6c7a14d69bbc7c53125cc03
terrycojones <terry@jon.es>
2023-05-31 14:59:50 +0200

    Added day-2 questionaire-counter.py code

---
+++ day-2/questionaire-counter.py
@@ -0,0 +1,33 @@
+import csv
+from collections import Counter
+
+nSkips = 3
+skipCount = 0
+stats = Counter()
+
+
+with open("sofia-questionaire.csv") as fp:
+    reader = csv.reader(fp)
+
+    while skipCount < nSkips:
+        next(reader)
+        skipCount += 1
+
+    for row in reader:
+        language1 = row[9].strip().lower()
+        if language1 and language1 != "n/a":
+            language, level = language1.split()
+            stats[language] += 1
+
+
+def printSummary():
+    total = stats.total()
+    for language in stats:
+        count = stats[language]
+        pct = count / total * 100.0
+        print(f"{language:15} {count:2} {pct:5.2f}%")
+
+    print("There are", total, "students.")
+
+
+printSummary()
